63322636bc28229fcfc5eb06259bf92b1e335c29
JoshCaneday <146874169+JoshCaneday@users.noreply.github.com>
2023-11-28 10:51:50 -0800

    commit

--- grade.sh
+++ grade.sh
@@ -14,3 +14,26 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+
+if [[ -f ./student-submission/ListExamples.java ]]
+    then
+        echo "File Name is Correct!"
+    else
+        echo "FILE NAME INCORRECT! No Such File Named: (ListExamples.java)"
+        exit
+fi
+
+cp -r ./student-submission/ListExamples.java ./grading-area/
+cp -r ./TestListExamples.java ./grading-area/
+
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" ./grading-area/TestListExamples.java ./grading-area/ListExamples.java
+
+
+if [[ $? -eq 0 ]]
+    then
+        echo "Success"
+    else
+        echo "Failed Did not work"
+fi
+
